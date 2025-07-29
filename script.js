b5f05587e219b03fefcdf2b8a1a33dfc45e359ff
chauffeurdeluxe <chauffeurdeluxe@yahoo.com>
2025-07-29 16:28:09 +1000

    Update script.js

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ document.getElementById('calculateFare').addEventListener('click', function () {
   };
 
   try {
-    const response = await fetch('https://server-qdh1.onrender.com:10000/create-checkout-session', {
+    const response = await fetch('https://server-qdh1.onrender.com/create-checkout-session', {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
   body: JSON.stringify(bookingData)
